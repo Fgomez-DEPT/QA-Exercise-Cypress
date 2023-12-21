da80a3cf2ba3287e57ecd331e0777eb89d7373c5
Fernando Gomez <87374079+Fgomez-DEPT@users.noreply.github.com>
2023-12-21 12:03:06 -0300

    Update depttest.cy.js

--- cypress/e2e/depttest.cy.js
+++ cypress/e2e/depttest.cy.js
@@ -1,6 +1,6 @@
 describe('QA testing spec', () => {
     it('passes', () => {
-        cy.intercept('GET', 'https://api.spacexdata.com/v3/rockets').as('rockets')
+        cy.intercept('GET', 'rockets').as('rockets')
         cy.visit('https://csb-x6dpt1.netlify.app/');
 
         cy.get('@rockets').should((req) => {
